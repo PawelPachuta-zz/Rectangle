83efbea5dfb90af114a4b75f952ee4d9b2bbd69a
Pawel Pachuta <46320624+PawelPachuta@users.noreply.github.com>
2020-01-14 08:25:10 +0000

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Rectangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Give a width: ");
+            string w = Console.ReadLine();
+            Console.Write("Give a height: ");
+            string h = Console.ReadLine();
+
+            int x = int.Parse(w);
+            int y = int.Parse(h);
+
+            Console.WriteLine(x + " " + y);
+
+            for (int i = 0; i < y; i++)
+            {
+                
+                for (int j = 0; j < x; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
